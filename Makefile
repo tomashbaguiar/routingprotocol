2a5d00b56365b09b2c5f71a9afa17b643d3fc9f1
Tomás <tomashbaguiar@ufmg.br>
2018-06-06 10:19:29 -0300

    first test

--- Makefile
+++ Makefile
@@ -1,12 +1,13 @@
 CC = gcc
 CFLAGS = -Wall -Werror -Wextra -pedantic -std=c11
 #LDFLAGS = -l json
+LDFLAGS = -pthread 
 
 all: router
 
 router: router.c
-	#$(CC) $(CFLAGS) router.c -o router $(LDFLAGS)
-	$(CC) $(CFLAGS) router.c -o router
+	$(CC) $(CFLAGS) router.c -o router $(LDFLAGS)
+	#$(CC) $(CFLAGS) router.c -o router
 
 clean:
 	rm router
--- router.c
+++ router.c
@@ -10,10 +10,13 @@
 #include <unistd.h>
 
 #include <json-c/json.h>
+#include <pthread.h>
 
 #define PORTA   55151                                                           // Porta padrão dos roteadores.
 #define MAX_IP  255
 #define IP_STD	"127.0.1."														// "IP" da rede.
+#define INF_COST 0xff
+#define NOLINK  -1
 
 //  Estrutura da tabela de roteamento   //
 struct tableEntry  {
@@ -34,16 +37,26 @@ struct topTable {
 //  Funções e procedimentos do protocolo    //
 void retrieve_command(char *command, char *ip, uint32_t *cost);                 // Procedimento que recebe os comandos.
 //struct tableEntry createTableEntry(uint32_t id, char *ipAddr, uint32_t nextHop, uint32_t cost);// Função que cria novo vetor de um nó.
-struct tableEntry createTableEntry(char *ipAddr, uint32_t nextHop, uint32_t cost);// Função que cria novo vetor de um nó.
-void updateTables(uint8_t src, uint8_t dst, uint8_t cst);                       // Atualiza uma entrada na tabela.
+//struct tableEntry createTableEntry(char *ipAddr, uint32_t nextHop, uint32_t cost);// Função que cria novo vetor de um nó.
+struct tableEntry createTableEntry(uint8_t nextHop, uint8_t cost);              // Função que cria novo vetor de um nó.
+//void updateTables(uint8_t src, uint8_t dst, uint8_t cst);                       // Atualiza uma entrada na tabela.
 void disableLink(uint8_t nodeID);                                               // Retira um vizinho.
-uint8_t getID(struct tableEntry node);                                          // Função que retorna o ID de um nó.
-void initTable(char *myIp);                                                     // Procedimento que inicializa a tabela do nó.
+uint8_t getID(char*);                                                           // Função que retorna o ID de um nó.
+void initTable();                                                               // Procedimento que inicializa a tabela do nó.
+void addLink(char *nodeIP, uint32_t cost);                                      // Procedimento que adiciona um arco a um nó.
 
+//  Procedimentos thread para recebimento/envio de atualizações na tabela   //
+void *ptSendUpdate(void *args);                                                 // Envia pacotes de atualização para os vizinhos.
+void *ptRecvData(void*);                                                        // Recebe datagramas.
 
 //  Variáveis globais   //
 struct topTable mySelf;                                                         // Guarda a tabela de roteamento deste nó.
 char *myIP = NULL;                                                              // Guarda o endereço IP deste nó.
+int myfd = 0;                                                                   // Guarda o descritor do socket.
+uint8_t neigh[MAX_IP] = {0};                                                    // Vetor que guarda a lista de adjacência.
+
+pthread_mutex_t lock;                                                           // Mutex para acesso à tabela de roteamento.
+uint8_t quit = 0;                                                               // Booleano para existência das threads.
 
 int main(int argc, char *argv[])                                                //  ./router <ADDR> <PERIOD> [STARTUP]
 {
@@ -52,11 +65,12 @@ int main(int argc, char *argv[])                                                
         fprintf(stderr, "Utilização:\t./router <ADDR> <PERIOD> [STARTUP]\n");
         return EXIT_FAILURE;
     }
-    myIp = malloc(strlen(argv[1] * sizeof(char)));
+    myIP = malloc(strlen(argv[1]) * sizeof(char));
     strcpy(myIP, argv[1]);
+    unsigned int period = (unsigned int) atoi(argv[2]);                         // Recebe o período de atualização da tabela.
 
     //  Inicializa o socket //
-    int myfd = socket(AF_INET, SOCK_DGRAM, 0);
+    myfd = socket(AF_INET, SOCK_DGRAM, 0);
     if(myfd == -1)    {                                                         // Verifica erros na criação do socket.
         perror("socket");
         return EXIT_FAILURE;
@@ -68,7 +82,6 @@ int main(int argc, char *argv[])                                                
     memset(&myAddr, 0, addrLen);
     myAddr.sin_family = AF_INET;                                                // Socket IPv4.
     myAddr.sin_port = htons(PORTA);                                             // Escuta na PORTA.
-    //myAddr.sin_addr.s_addr = inet_addr(argv[1]);                                // Endereço IP do roteador.
     myAddr.sin_addr.s_addr = inet_addr(myIP);                                   // Endereço IP do roteador.
 
     //  Fixa socket a porta //
@@ -78,19 +91,44 @@ int main(int argc, char *argv[])                                                
     }
 
     //  Inicia a tabela de roteamento   //
-    initTable(myIP);
+    initTable();
+
+    //  Inicializa os mutexes   //
+    if(pthread_mutex_init(&lock, NULL) != 0)    {                               // Verifica erro na inicialização.
+        perror("init-tmutex");
+        return EXIT_FAILURE;
+    }
 
     //  Inicia o loop de recebimento de comandos    //
+    pthread_mutex_lock(&lock);                                                  // Bloqueia o mutex antes de iniciar o loop.
     while(1)    {
+                
+        //  Chama as threads de envio e recebimento //
+        pthread_t stid, rtid;                                                   // Variáveis de pthread.
+        if(pthread_create(&stid, NULL, &ptSendUpdate, (void *) &period) != 0) { // Verifica erro.
+            perror("sUpdate-create");
+            return EXIT_FAILURE;
+        }
+        if(pthread_create(&rtid, NULL, &ptRecvData, NULL) != 0)   {             // Verifica erro.
+            perror("sUpdate-create");
+            return EXIT_FAILURE;
+        }
+        
         //  Recebe o comando    //
         char command[6] = {0};
         char ip[15] = {0};
         uint32_t weight = 0;
         retrieve_command(command, ip, &weight);
+printf("comando = -%s-%s-%hu.\n", command, ip, weight);
 
         //  Realiza a operação desejada //
         if(!strcmp("quit", command)) {
             fprintf(stdout, "Saindo...\n");
+            quit = 1;                                                           // Sinal para destruir threads.
+            if(pthread_join(stid, NULL) != 0)
+                perror("join-sender");
+            if(pthread_join(rtid, NULL) != 0)
+                perror("join-receiver");
             break;
         }
         else if(!strcmp("add", command)) {                                      // Operação de adição de arco.
@@ -111,7 +149,7 @@ int main(int argc, char *argv[])                                                
         else if(!strcmp("trace", command))  {
             //  Verifica para quem mandar a mensagem    //
             //
-            /****** busca na tabela deste nó (mySelf.table[getID(myIP)][i]) proximo nó para dst ip. */
+            /****** busca na tabela deste nó (mySelf.table[myID][i]) proximo nó para dst ip. */
 
             //  Envia a mensagem    //
             //
@@ -130,7 +168,10 @@ int main(int argc, char *argv[])                                                
             fprintf(stderr, "\tadd <ip> <weight>\n\tdel <ip>\n\ttrace <ip>\n");
             fprintf(stderr, "\tquit\n");
         }
+        pthread_mutex_unlock(&lock);                                            // Desbloqueia o mutex da tabela.
     }
+
+    pthread_mutex_destroy(&lock);                                               // Destrói o mutex.
 
     close(myfd);
 
@@ -180,9 +221,7 @@ struct tableEntry createTableEntry(uint8_t nextHop, uint8_t cost)
 {
     struct tableEntry new;
     new.cost = cost;
-    //new.id = getID(ipAddr);
     new.nextHop = nextHop;
-    //new.ipAddr = ipAddr;
     return new;
 }
 
@@ -197,8 +236,11 @@ void updateTables(uint8_t src, uint8_t dst, uint8_t cst)
 
 void addLink(char *nodeIP, uint32_t cost)
 {
+    pthread_mutex_lock(&lock);                                                  // Bloqueia o mutex antes de escrever.
 	mySelf.table[getID(myIP)][getID(nodeIP)] = createTableEntry(getID(nodeIP), cost);
 	mySelf.table[getID(nodeIP)][getID(myIP)] = createTableEntry(getID(myIP), cost);
+    neigh[getID(nodeIP)] = 1;
+    pthread_mutex_unlock(&lock);                                                // Desbloqueia o mutex depois de escrever.
 }
 
 void disableLink(uint8_t nodeID)
@@ -206,8 +248,11 @@ void disableLink(uint8_t nodeID)
     //  Retira vetor do node ID da tabela    //
     //mySelf.table[getID(myIP)][nodeID].cost = INF_COST;
     //mySelf.table[getID(myIP)][nodeID].nextHop = NOLINK;
+    pthread_mutex_lock(&lock);                                                  // Bloqueia o mutex antes de escrever.
     mySelf.table[getID(myIP)][nodeID] = createTableEntry(NOLINK, INF_COST);
     mySelf.table[nodeID][getID(myIP)] = createTableEntry(NOLINK, INF_COST);
+    neigh[nodeID] = 0;
+    pthread_mutex_unlock(&lock);                                                // Desbloqueia o mutex depois de escrever.
 }
 
 void initTable()
@@ -219,12 +264,77 @@ void initTable()
 
     //  Cria entrada do nó  //
     uint8_t myID = getID(myIP);
-    //struct tableEntry me = createTableEntry(myID, myIp, myID, 0);
-    //struct tableEntry me = createTableEntry(myIP, myID, 0);
     struct tableEntry me = createTableEntry(myID, 0);
-    //me.ipAddr = myIp;    
 
     //  Adiciona este nó à tabela    //
     mySelf.table[myID][myID] = me;
+}
+
+void *ptSendUpdate(void *args)
+{
+    unsigned int period = *((unsigned int *) args);                             // Converte o argumento.
+
+    //  Cria mensagem de update //
+    struct sockaddr_in addr;
+    socklen_t addrLen = sizeof(addr);
+    memset(&addr, 0, addrLen);
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(PORTA);
+
+    struct tableEntry table[MAX_IP];
+    while(!quit)    {
+        sleep(period);
+        pthread_mutex_lock(&lock);
+        for(uint8_t i = 0; i < MAX_IP; i++)    {
+            if(neigh[i] == 1)   {
+                addr.sin_addr.s_addr = inet_addr(getIP(i));
+                for(uint8_t j = 0; j < MAX_IP; j++)
+                    table[j] = mySelf.table[mySelf.myID][j];
+
+
+                char msg[] = "update";
+                if(sendto(myfd, (char *) msg, strlen(msg) * sizeof(char), 0, (struct sockaddr *) &addr, addrLen) == -1)  {
+                    perror("sendto1");
+                    pthread_exit(NULL);
+                }
+
+
+                if(sendto(myfd, (struct tableEntry *) table, MAX_IP * sizeof(struct tableEntry), 0, (struct sockaddr *) &addr, addrLen) == -1)  {
+                    perror("sendto");
+                    pthread_exit(NULL);
+                }
+            }
+        }
+        pthread_mutex_unlock(&lock);
+    }
+    pthread_exit(NULL);
+}
+
+void *ptRecvData(void* args)
+{
+    struct sockaddr_in addr;
+    socklen_t addrLen = sizeof(addr);
+
+    while(!quit)    {
+        char msg[10] = {0};
+        ssize_t recvd = recvfrom(myfd, (char *) msg, 10 * sizeof(char), 0, (struct sockaddr *) &addr, &addrLen);
+        if(recvd <= 0)  {
+            perror("recvfrom1");
+            pthread_exit(NULL);
+        }
+    
+        struct tableEntry table[MAX_IP];
+        recvd = recvfrom(myfd, (struct tableEntry *) table, MAX_IP * sizeof(struct tableEntry), 0, (struct sockaddr *) &addr, &addrLen);
+        if(recvd <= 0)  {
+            perror("recvfrom");
+            pthread_exit(NULL);
+        }
+        pthread_mutex_lock(&lock);
+
+        fprintf(stdout, "Received:[%s] \tfrom %s.\n", msg, inet_ntoa(addr.sin_addr));
+
+        pthread_mutex_unlock(&lock);
+    }
+    pthread_exit(args);
 }
 
